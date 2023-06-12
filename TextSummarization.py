4da3e71bf30e2dda647a5ecf4e62ec411af122f5
bezgaliba <mpsamarels@gmail.com>
2023-06-12 17:15:57 +0300

    sentences normalization

--- TextSummarization.py
+++ TextSummarization.py
@@ -1,13 +1,30 @@
-from flask import Flask, request
-from flask_cors import CORS
+# from flask import Flask, request
+# from flask_cors import CORS
+import nltk
+import re
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route('/submit')
-def submit():
-    value = request.args.get('value')
-    return f"I have submitted: {value}"
+# @app.route('/submit')
 
-if __name__ == '__main__':
-    app.run()
+# def submit():
+#     value = input("Ievadiet apstrādājamo tekstu: ")
+#     normalized_text = normalize_text(value)
+#     return f"Normalized text:\n{normalized_text}"
+
+def normalize_text(text):
+    sentences = nltk.sent_tokenize(text)
+    # Teksta vienības normalizēšana (lowercase)
+    normalized_sentences = [sentence.lower() for sentence in sentences]
+    # Teksta vienības normalizēšana (noņemt nenozīmīgus simbolus)
+    normalized_sentences = [re.sub(r'\W+', ' ', sentence) for sentence in normalized_sentences]
+    normalized_text = '\n'.join(normalized_sentences)
+    return normalized_text
+
+value = input("Enter the text to be processed: ")
+normalizedText = normalize_text(value)
+print(f"Normalized text:\n{normalizedText}")
+
+# if __name__ == '__main__':
+#     app.run()
