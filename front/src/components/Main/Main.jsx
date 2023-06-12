842137b1597e41294c9293e1f674c9f6ecb5006d
bezgaliba <mpsamarels@gmail.com>
2023-06-12 17:35:26 +0300

    add response handling and style

--- front/src/components/Main/Main.jsx
+++ front/src/components/Main/Main.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Main.css'
 
 function Main() {
   const [value, setValue] = useState('');
+  const [data, setData] = useState();
+  const [response, setResponse] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.get(`http://localhost:5000/submit?value=${value}`);
-      console.log(response.data);
+      const data = response.data;
+      setData(data)
+      setResponse(true);
     } catch (error) {
       console.error(error);
     }
@@ -19,10 +24,14 @@ function Main() {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          className='textbox'
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
         <button type="submit">Submit</button>
+        { response && (
+          <p><b>RECEIVED RESPONSE:</b><br></br>{data}</p>
+        )}
       </form>
     </div>
   );
