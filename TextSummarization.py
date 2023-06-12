324a1bcae0a77958ef508b423019a78078d66cb4
bezgaliba <mpsamarels@gmail.com>
2023-06-12 17:52:24 +0300

    create tokens and calculate its frequencies

--- TextSummarization.py
+++ TextSummarization.py
@@ -2,6 +2,7 @@
 # from flask_cors import CORS
 import nltk
 import re
+import collections
 
 # app = Flask(__name__)
 # CORS(app)
@@ -13,6 +14,31 @@ import re
 #     normalized_text = normalize_text(value)
 #     return f"Normalized text:\n{normalized_text}"
 
+# TODO
+# Katram tokenam pievienot vērtību.
+# 2 veidi: 
+# papildināt frequency funkciju {token}: {count}, {VERTIBA}
+# jaunā funkcijā izveidot jaunu array {token}: {value}
+
+
+def frequency(tokenObject):
+    token_counts = collections.Counter(tokenObject)
+    frequency_results = []
+    for token, count in token_counts.items():
+        frequency_results.append(f"{token}: {count}, {VERTIBA}")
+    return frequency_results
+
+def tokenize_text(text):
+    tokens = nltk.word_tokenize(text)
+    return tokens
+
+# TODO
+# Noņemt insignificant vārdus kā 'have', 'me', 'PM' etc.
+# Maybe Latviski?
+
+#def significator(text): 
+
+
 def normalize_text(text):
     sentences = nltk.sent_tokenize(text)
     # Teksta vienības normalizēšana (lowercase)
@@ -22,9 +48,21 @@ def normalize_text(text):
     normalized_text = '\n'.join(normalized_sentences)
     return normalized_text
 
-value = input("Enter the text to be processed: ")
+# MAIN FUNKCIJA
+
+value = input("Ievadiet apstrādājamo tekstu: ")
+
 normalizedText = normalize_text(value)
-print(f"Normalized text:\n{normalizedText}")
+print(f"Normalizēts teksts:\n{normalizedText}")
+
+normalizedText = significator(value)
+print(f"Normalizēts teksts:\n{normalizedText}")
+
+tokenizedText = tokenize_text(normalizedText)
+print(f"Tokeni:\n{tokenizedText}")
+
+frequentedText = frequency(tokenizedText)
+print(f"Absolūtais biežums:\n{frequentedText}")
 
 # if __name__ == '__main__':
 #     app.run()
